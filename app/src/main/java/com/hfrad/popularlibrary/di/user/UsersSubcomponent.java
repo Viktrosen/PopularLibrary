c3951bdd74a259876b9c77ee57c6835a86a74061
Виктор <bolotov.vit@yandex.ru>
2021-06-09 02:03:39 +0500

    Урок 2. Написал тесты для презентера

--- app/src/main/java/com/hfrad/popularlibrary/di/user/UsersSubcomponent.java
+++ app/src/main/java/com/hfrad/popularlibrary/di/user/UsersSubcomponent.java
@@ -3,7 +3,7 @@ package com.hfrad.popularlibrary.di.user;
 import dagger.Subcomponent;
 import com.hfrad.popularlibrary.di.repository.RepositorySubcomponent;
 import com.hfrad.popularlibrary.di.user.module.UsersModule;
-import com.hfrad.popularlibrary.mvp.presenter.UsersPresenter;
+import com.hfrad.popularlibrary.mvp.presenter.NumberPresenter;
 import com.hfrad.popularlibrary.ui.adapter.UserRVAdapter;
 
 @UsersScope
@@ -15,6 +15,6 @@ import com.hfrad.popularlibrary.ui.adapter.UserRVAdapter;
 public interface UsersSubcomponent {
     RepositorySubcomponent repositorySubComponent();
 
-    void inject(UsersPresenter usersPresenter);
+    void inject(NumberPresenter numberPresenter);
     void inject(UserRVAdapter adapter);
 }
